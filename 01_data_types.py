e49ea6a23630eebf06edf68b866ba471b5ee27c8
Meya Middleton <mmiddleton2@brooklynsteamcenter.org>
2025-09-12 09:41:22 -0400

    hi

--- 01_data_types.py
+++ 01_data_types.py
@@ -1,5 +1,30 @@
 #TASK 1:
-#Create a float variable, and then convert it to an integer
-#Print both the original variable and the converted integer.
+#Firstly, I created my float variable. This variable is our tester.
+my_float = 7.89
 
-my_string = "14.56, 21.11, 11.10, "
+#Next, I made a variable equal to the integer and in the space I put the float variable inside.
+my_int = int(my_float)
+
+#Finally I printed the original float and the converted integer and printed this to the console. The console should show the conversion between the original float and the finale result it being a integer. The int function truncates the decimal part.
+print(my_float)
+print(my_int)
+
+#Task 2:
+
+#Task 3:
+
+#Task 4:
+
+#TASK 5:
+
+my_str = ["2", "4", "6", "8", "10"]
+my_tupple  =tuple(my_str)
+print(my_str)
+print(my_tupple)
+
+
+#TASK 6:
+
+subjects = ["English", "Science", "History"]
+print(subjects)
+
