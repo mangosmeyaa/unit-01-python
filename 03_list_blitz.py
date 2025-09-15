f4734ec523f8277913ff1131c6c49c585ad8e937
Meya Middleton <mmiddleton2@brooklynsteamcenter.org>
2025-09-15 10:50:22 -0400

    hehehee

---
+++ 03_list_blitz.py
@@ -0,0 +1,39 @@
+print("Task 1")
+print("-----------------")
+#Made my list of 4 items of my choice.
+my_list = ["Bananas", "Tomatoes", 354, False]
+print(my_list)
+
+print("Task 2")
+print("-----------------")
+#Appended another item so it adds to the list.
+my_list.append("Turkeys, Hotdogs, Ice Cream")
+print(my_list)
+
+print("Task 3")
+print("-----------------")
+del my_list[1] 
+#I deleted Tomatoes off my list so it won't show
+print(my_list)
+
+print("Task 4")
+print("-----------------")
+#Modified the third which was tomatoes ig
+my_list[3] = "Chupapi"
+print(my_list)
+
+print("Task 5")
+
+
+#Task 6
+del my_list[2]
+
+#Task 7
+
+
+
+
+
+
+
+
