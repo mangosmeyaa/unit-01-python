bdf373f2bd61b636a70b83b330f8881af0912452
Meya Middleton <mmiddleton2@brooklynsteamcenter.org>
2025-09-16 10:24:39 -0400

    wtv this is ig

--- 03_list_blitz.py
+++ 03_list_blitz.py
@@ -1,33 +1,39 @@
 print("Task 1")
-print("-----------------")
 #Made my list of 4 items of my choice.
 my_list = ["Bananas", "Tomatoes", 354, False]
 print(my_list)
 
-print("Task 2")
 print("-----------------")
+
+print("Task 2")
 #Appended another item so it adds to the list.
-my_list.append("Turkeys, Hotdogs, Ice Cream")
+my_list.append("Turkeys")
 print(my_list)
+
+print("-----------------")
 
 print("Task 3")
-print("-----------------")
 del my_list[1] 
-#I deleted Tomatoes off my list so it won't show
+#I deleted Tomatoes off my list so it won't show in the list.
 print(my_list)
 
-print("Task 4")
 print("-----------------")
+
+print("Task 4")
 #Modified the third which was tomatoes ig
 my_list[3] = "Chupapi"
 print(my_list)
 
+print("-----------------")
+
 print("Task 5")
 
 
-#Task 6
-del my_list[2]
 
+
+print("Task 6")
+del my_list[2]
+print("-----------------")
 #Task 7
 
 
