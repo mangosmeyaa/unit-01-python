05237446605fc77e0e4d39925a2dfa8de0c0b7ee
Meya Middleton <mmiddleton2@brooklynsteamcenter.org>
2025-09-17 10:46:34 -0400

    ooos resubmit

--- 04_condtional_calisthenics.py
+++ 04_condtional_calisthenics.py
@@ -28,15 +28,15 @@ A=5#create varable for zone A and zone B
 B=7
 
 if weight <= 0:
-    #First, I created the error if thr weight weight is less than or equal to zero.
+    #First, I created the error if the weight weight is less than or equal to zero.
     print("Error")
 
-if zone == "A" or zone == "zone A":#Second, I created if statements for Zone A as well as for Zone . These statements will do operations.
+if zone == "A" or zone == "zone A":#Second, I created if statements for Zone A as well as for Zone. These statements will do operations.
     weight * A
     print(weight * A)
 elif zone == "B" or zone == "zone B":
     weight * B
     print(weight * B)
-else:#I created the else statement just in the case that the user does not pick Zone A or Zone B
+else:#I created the else statement just in the case that the user does not pick Zone A or Zone B.
     print("pick a zone")
 
