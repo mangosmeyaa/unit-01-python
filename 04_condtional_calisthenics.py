b1253e71bb75108bded63bd1b378aab5849bbea0
Meya Middleton <mmiddleton2@brooklynsteamcenter.org>
2025-09-17 10:03:01 -0400

    done ig

--- 04_condtional_calisthenics.py
+++ 04_condtional_calisthenics.py
@@ -20,3 +20,24 @@ if my_user in my_fruits:
     print("Yes, that fruit is in the list")
 else:
     print("No, that fruit is not in the list.")
+
+print("Task 5")
+weight=int(input("What is the weight: "))#create variable for weight integer and zone string
+zone=str(input("Which zone are you taking: "))
+A=5#create varable for zone A and zone B
+B=7
+
+if weight <= 0:#first create error if weight is less than or equal to zero
+    print("Error")
+
+if zone == "A" or zone == "zone A":#then create if statements for zone a and zone B
+    weight * A
+    print(weight * A)
+elif zone == "B" or zone == "zone B":
+    weight * B
+    print(weight * B)
+else:#create else statement if user does not pick zone A or B
+    print("pick a zone")
+
+
+print("Task 6")
