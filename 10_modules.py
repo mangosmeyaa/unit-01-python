6739fe2aa566f1f8fd0885234fcf13b9143f74e7
Meya Middleton <mmiddleton2@brooklynsteamcenter.org>
2025-10-08 10:48:37 -0400

    hyy twin

--- 10_modules.py
+++ 10_modules.py
@@ -1,46 +1,75 @@
 import os
 import sys
 
-#Task 1
-import os
-
-# Get and print the current working directory
+# First thing I was was create a variable set to the command. What the os.getcwd() does is that it checks to see which directory is running and checks for that.
+print("Task 1")
+print()
 current_directory = os.getcwd()
 print("Current working directory:", current_directory)
+# It prints to the console showing what directory it is then it finds it within the console.
+
+print()
+print("Task 2")
+print()
+# I created a new folder, and used a another os command called makedirs. This command allows us to create new directories. I added the exist_ok because that is a boolean it checks to see if it exists. If it exists, it's "True".
+os.makedirs('test_folder', exist_ok=True)
+
+# List all files and directories in the current directory
+items = os.listdir('.')
+
+#This print the list, to display all the files/directory. Added a for loop so it says the condition everytime.
+print("Files and directories in current directory:")
+for item in items:
+    print(item)
 
 
 
-"""
-Task 2 (os module):
-Create a new directory called "test_folder" in the current directory.
-Then print a list of all files and directories in the current directory.
-"""
+print()
+print("Task 3")
+print()
+
+#I created a conditional, and used os.path to check if a given path point exists within my directory.
+#If when it checks if data is a directory and its true, it returns that it exists already.
+if os.path.isdir("data") == True:
+    print("This directory already exists silly")
+#I created an else statement because what if it doesn't exists? So it will say it doesn't exist. Then the user is asked to input that no and it automically creates it for u.
+#This mkdir checks a specific directory to see if it's there.
+#Prints this all to the console.   
+else: 
+    print("Wasn't even created. Directory doesn't exists.")
+    creation = input("You want to create directory?")
+    if creation == "Yes":
+        os.mkdir("data")
+        print("Directory created")
+
+print()
+print("Task 4")
+print()
+
+# We are using the os to get the current directory .
+#Prints this and fetches it, then creating the conditional statement.
+os.getcwd
+print(os.getcwd())
+os.listdir()
+#This conditional looks for this directory, if it doesn't exists in it's path it prints an error message saying it can 't be found.
+#If it was found within it's path it prints it was found.
+if not os.path.exists("config.txt"):
+    print("This file can't be found")
+else:
+    print("File was FOUNDD!!")
 
 
-"""
-Task 3 (os module):
-Write a program that checks if a directory called "data" exists in the current 
-working directory. If it doesn't exist, create it. If it does exist, print 
-"Directory already exists."
-"""
+print()
+print("Task 5")
+print()
 
+#Using the sys version command to print the version it's in. Simple. 
+print(sys.version)
 
-"""
-Task 4 (os.path module):
-Write a program that checks if a file called "config.txt" exists in the current directory.
-If it exists, print its path. If it doesn't exist, print "File not found."
-"""
+print()
+print("Task 6")
+print()
 
-
-"""
-Task 5 (sys module):
-Write a program that prints the Python version you are currently using.
-"""
-
-
-"""
-Task 6 (sys module):
-Write a program that prints the platform your Python interpreter is running on
-(e.g., 'linux', 'win32', 'darwin'). The output should be user friendly names
-"Linux", "Windows", "MacOS"
-"""
+#This checks if it's friendly. If it's not it changes it back to a friendly name.
+if sys.platform.lower() == "darwin":
+    print("MacOS")
